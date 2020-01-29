7c6f364dd19a1ff4bb4ac3dc3469f0cf0e046e8c
ajmengistu <a_mengistu@outlook.com>
2020-01-28 20:05:54 -0500

    Added initial backend spring server code connected to a mysql db

---
+++ backend/src/main/java/com/ecommerce/bookstore/BookstoreApplication.java
@@ -0,0 +1,21 @@
+package com.ecommerce.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@GetMapping("/greeting")
+	public String getHome(){
+		return "hello world!!";
+	}
+
+}
