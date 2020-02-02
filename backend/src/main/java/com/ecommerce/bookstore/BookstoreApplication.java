0483ba1b0f868777341c34abe666aae03a42105d
ajmengistu <a_mengistu@outlook.com>
2020-02-02 16:21:00 -0500

    Removed frontend reactjs with typescript starter coode in order to switch to reactjs with javascript for convenience

--- backend/src/main/java/com/ecommerce/bookstore/BookstoreApplication.java
+++ backend/src/main/java/com/ecommerce/bookstore/BookstoreApplication.java
@@ -8,9 +8,11 @@ import com.ecommerce.bookstore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @SpringBootApplication
 public class BookstoreApplication {
